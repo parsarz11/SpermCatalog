2d918a1c91777d5fb86ab87ae14e8333d5f25604
parsarz11 <p.razaghan1387@gmail.com>
2024-01-12 16:33:56 +0330

    Add Dependencies Extension method

---
+++ SpermCatalog.API/Extenssions/Dependencies.cs
@@ -0,0 +1,16 @@
+﻿using SpermCatalog.DataAccess.Contracts;
+using SpermCatalog.DataAccess.Repositories;
+
+namespace SpermCatalog.API.Extenssions
+{
+    public static class Dependencies
+    {
+        public static IServiceCollection AddDependencies(this IServiceCollection Services)
+        {
+            Services.AddScoped<IDairySperm , DairySpermRepository>();
+            Services.AddScoped<IBeefRepository , BeefSpermRepository>();
+
+            return Services;
+        }
+    }
+}
--- SpermCatalog.API/Program.cs
+++ SpermCatalog.API/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SpermCatalog.DataAccess.DatabaseContext;
-
+using SpermCatalog.API.Extenssions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +10,9 @@ builder.Services.AddDbContext<SpermCatalogDbContext>(config =>
         config.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
     }
 );
+
+builder.Services.AddDependencies();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
