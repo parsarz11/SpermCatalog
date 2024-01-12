638ce578465d2c176b6ee241c02d3b3c2d675a32
parsarz11 <p.razaghan1387@gmail.com>
2024-01-12 15:17:12 +0330

    inject DbContext and add connection string and Add IBeefRepo

---
+++ SpermCatalog.DataAccess/DatabaseContext/SpermCatalogDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpermCatalog.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpermCatalog.DataAccess.DatabaseContext
+{
+    public class SpermCatalogDbContext : DbContext
+    {
+        public SpermCatalogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BeefSperm> BeefSperms { get; set; }
+        public DbSet<DairySperm> DairySperms { get; set; }
+    }
+}
