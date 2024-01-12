55fcd59a6c2b1868c2a368c04c5a73e539c15776
parsarz11 <p.razaghan1387@gmail.com>
2024-01-12 14:52:00 +0330

    create sperms entities

---
+++ SpermCatalog.DataAccess/Entities/BeefSperm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpermCatalog.DataAccess.Entities
+{
+    public class BeefSperm
+    {
+        public int id { get; set; }
+        public string RegNo { get; set; }
+        public string BREED { get; set; }
+        public string NAME { get; set; }
+
+        public double SCE { get; set; }
+        public double CR { get; set; }
+        public double DM { get; set; }
+        public double PCAR { get; set; }
+        public double RDT { get; set; }
+        public double CONF { get; set; }
+        public double COUL { get; set; }
+        public double GRAS { get; set; }
+        public double IAB { get; set; }
+        public double ICRC { get; set; }
+
+        public string SIRE { get; set; }
+        public string MGS { get; set; } 
+        public int Price { get; set; }
+
+    }
+}
