55b9041695c13df79d483da7620e5ffb73355f22
parsarz11 <p.razaghan1387@gmail.com>
2024-04-14 16:27:42 +0330

    a huge commit that migrate database from sql server to mongo db, change csvDto to AddDto and set default value for custom order,add breed property for dairySperms in Dtos , change type of id from int to string,change name of add range methods and change parameter from add dto to entity and handle mapping in other places , change Id filters beacuse of type changeing and add breed filter in filter method, fix descending order bug,get db settings from program file,rewrite repositories for mongo db commands

---
+++ SpermCatalog.API/models/DTOs/AddDTOs/AddDairySpermDTO.cs
@@ -0,0 +1,39 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace SpermCatalog.API.models.DTOs.csvDTOs
+{
+    public class AddDairySpermDTO
+    {
+
+        public string RegNo { get; set; }
+        public string NAAB_CODE { get; set; }
+        public string NAME { get; set; }
+        public string Breed { get; set; }
+
+        public double ICC { get; set; }
+        public double LNM { get; set; }
+        public double FM { get; set; }
+        public double MILK { get; set; }
+        public double FAT { get; set; }
+        public double PRO { get; set; }
+        public double SCE { get; set; }
+        public double PL { get; set; }
+        public double DPR { get; set; }
+        public double PTAT { get; set; }
+        public double UDC { get; set; }
+        public double FLC { get; set; }
+        public double FS { get; set; }
+        public double FI { get; set; }
+        public double TPI { get; set; }
+
+
+
+        public string SIRE { get; set; }
+        public string MGS { get; set; }
+        public int price { get; set; }
+
+
+        public bool IsNew { get; set; }
+        public int CustomOrder { get; set; };
+    }
+}
